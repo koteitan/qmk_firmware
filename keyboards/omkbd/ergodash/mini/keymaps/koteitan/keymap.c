286a9d40b45ba0f94715dc594bc0fa30af69d385
koteitan <ukaibutton@mac.com>
2024-12-01 16:56:24 +0900

    Del is onto raise of BS/the number order is 01234 56789

--- keyboards/omkbd/ergodash/mini/keymaps/koteitan/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/koteitan/keymap.c
@@ -31,16 +31,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                 KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                 KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT ,
-    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SPC,                  KC_BSPC, KC_N,    KC_M,    KC_LT,   KC_GT,   KC_PGUP, KC_BSLS,
-    KC_LALT, KC_DEL,  KC_TAB,      RAISE,        IME,     KC_SPC, KC_SPC, KC_BSPC, KC_BSPC, KC_ENT,      LOWER,        KC_HOME, KC_PGDN, KC_END
+    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SPC,                  KC_DEL,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_PGUP, KC_BSLS,
+    KC_LALT, KC_LGUI, KC_TAB,      RAISE,        IME,     KC_SPC, KC_SPC, KC_DEL , KC_DEL,  KC_ENT,      LOWER,        KC_HOME, KC_PGDN, KC_END
   ),
 
   /* Raise */
   [_RAISE] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                 KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                 KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT ,
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC,                  KC_BSPC, KC_N,    KC_M,    KC_LT,   KC_GT,   KC_PGUP, KC_BSLS,
-    KC_LALT, KC_F11 , KC_TAB,      RAISE,        IME,     KC_SPC, KC_SPC, KC_BSPC, KC_BSPC, KC_ENT,      LOWER,        KC_HOME, KC_PGDN, KC_END
+    KC_TILD, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_LCBR,                 KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT ,
+    KC_LSFT, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_SPC,                  KC_DEL , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_PGUP, KC_BSLS,
+    KC_LALT, KC_F11 , KC_TAB,      RAISE,        IME,     KC_SPC, KC_SPC, KC_DEL , KC_DEL , KC_ENT,      LOWER,        KC_HOME, KC_PGDN, KC_END
   ),
 
   /* Adjust
